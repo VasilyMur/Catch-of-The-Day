b105f5eb852b5411e35b62284540a518d6d665d6
Vasily Muravyev <vmouravi@gmail.com>
2018-03-24 09:56:53 +0300

    Events, Refs and Binding (constructor for a method vs arrow function for a property of a Component)

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,13 +3,33 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    // Property 1 on component StorePicker
+    myInput = React.createRef();
+
+    // Property 2 on components StorePicker - Declare Method goToStore()
+    // goToStore(e) {
+    //     e.preventDefault();
+    //     console.log(this.myInput.current.defaultValue) 
+    // }
+
+    // Set Property rather than declare Method!!! not to use bind in constructor!!!
+    goToStore = (e) => {
+        e.preventDefault();
+        console.log(this.myInput.current.defaultValue) 
+    }
+
     render() {
         return (
             <React.Fragment>
                 { /* comment */ }
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter a Store</h2>
-                    <input type="text" required placeholder="Store Name"value={getFunName()}/>
+                    <input ref={this.myInput} type="text" required placeholder="Store Name"defaultValue={getFunName()}/>
                     <button type="submit">Visit Store</button>
                 </form>
             </React.Fragment>
