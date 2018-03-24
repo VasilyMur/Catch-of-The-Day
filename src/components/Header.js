8148d27f6e9a9be8d24e54e1307f6fba35fd83da
Vasily Muravyev <vmouravi@gmail.com>
2018-03-24 09:16:37 +0300

    Routing with React Router, Stateless Functional Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-                <header className="top">
+
+//Stateless Functional Component
+const Header = (props) => {
+    return (
+        <header className="top">
                     <h1>Catch 
                         <span className="ofThe">
                             <span className="of">Of</span>
@@ -11,11 +12,11 @@ class Header extends React.Component {
                         </span>
                         Day</h1>
                     <h3 className="tagline">
-                        <span>Fresh Daily</span>
+                        <span>{props.tagline}</span>
                     </h3>
                 </header>
-        )
-    }
+    );
 }
+
 
 export default Header;
