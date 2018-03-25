93d06badc0df8f6e1123623df22e093e098e6de4
Vasily Muravyev <vmouravi@gmail.com>
2018-03-25 16:25:18 +0300

    Load data into State onClick. Display State with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
 
@@ -27,14 +29,34 @@ class App extends React.Component {
         })
     }
 
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
+
+
+
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+
+                        {Object.keys(this.state.fishes).map(key => {
+                            return <Fish key={key} details={this.state.fishes[key]} />
+                        })}
+
+                    </ul>
+                  
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish}/>
+                <Inventory 
+                    loadSampleFishes={this.loadSampleFishes}
+                    addFish={this.addFish}
+                    />
             </div>
         )
     }
