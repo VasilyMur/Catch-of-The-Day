26f00a5bbbbe6f4b74aa38c6b0875eaf5f053885
Vasily Muravyev <vmouravi@gmail.com>
2018-03-26 11:30:59 +0300

    Update Order Status (Order State)

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,6 @@ class App extends React.Component {
 
         // 2. Add our new fish to that fishes variable
         fishes[`fish${Date.now()}`] = fish;
-        console.log('Adding a Fish')
 
         // 3. Set the new fishes object to State
         this.setState({
@@ -35,9 +34,20 @@ class App extends React.Component {
         })
     }
 
+    addToOrder = (key) => {
 
+        // 1. Take a copy of state
+        const order = {...this.state.order};
 
-
+        // 2. Either add to the order or update the number in our order
+        order[key] = order[key] + 1 || 1; 
+ 
+        // 3. Call Setstate to update our State object
+        this.setState({ 
+            order: order
+        })
+    }
+    
     render() {
         return (
             <div className="catch-of-the-day">
@@ -46,7 +56,12 @@ class App extends React.Component {
                     <ul className="fishes">
 
                         {Object.keys(this.state.fishes).map(key => {
-                            return <Fish key={key} details={this.state.fishes[key]} />
+                            return <Fish 
+                                key={key} 
+                                index={key}
+                                details={this.state.fishes[key]} 
+                                addToOrder={this.addToOrder}
+                            />
                         })}
 
                     </ul>
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
-    
+
     render() {
         const { image, name, desc, price, status } = this.props.details;
+        const isAvailable = status === 'available';
+
         return ( 
         
             <li className="menu-fish">
@@ -13,7 +15,7 @@ class Fish extends React.Component {
                     <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button>Add to Cart</button>
+                <button onClick={() => {this.props.addToOrder(this.props.index)}} disabled={!isAvailable}>{isAvailable ? 'Add to Cart' : 'Sold Out!'}</button>
             </li>
   
         )
